86391a98d7d9eb4692c6a653dab71c518ec01e7f
ricar <ricardocisneros197@gmail.com>
2023-01-12 15:35:01 -0800

    connected to the open weather api

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,21 @@
 var apiKey = "bec6baa443da28a01e0f2cd9b39dc903";
-
+ var l ='desert hot springs'
 function getApi(){
-    var requestUrl = ' https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={apiKey}';
+    var requestUrl = ` https://api.openweathermap.org/data/2.5/forecast?q=${l}&appid=${apiKey}`;
 
     fetch(requestUrl)
     .then(function(response) {
         return response.json();
+       
     })
     .then(function(data){
         console.log(data)
+        
+
+        
     })
+    
 }
+
+
+getApi()
