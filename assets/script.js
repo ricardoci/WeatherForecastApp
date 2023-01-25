32bf76dca138d3d839b5499e675ea5cad708ad45
Ricardo Cisneros <ricardocisneros197@gmail.com>
2023-01-24 23:58:52 -0800

    finished getting back data for the current day, now I am adding the 4 day forecast

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,13 @@
 const apiKey = "bec6baa443da28a01e0f2cd9b39dc903";
 const searchBar = document.querySelector('#search-bar');
 const searchButton = document.querySelector('#search-button');
+const one = document.querySelector('.one');
+const two = document.querySelector('.two');
+const three = document.querySelector('.three');
+const four = document.querySelector('.four');
 
 let weatherData;
+let forecastData;
 
 searchButton.addEventListener('click', getWeather);
 
@@ -16,6 +21,7 @@ function getWeather(event) {
     }
 
     const requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${apiKey}&units=imperial&limit=5&sort=forecast`;
+    const requestUrl3 = `https://api.openweathermap.org/data/2.5/forecast?q=${query}&appid=${apiKey}`
     searchBar.value = '';
 
     fetch(requestUrl)
@@ -28,27 +34,69 @@ function getWeather(event) {
         .then(data => {
             weatherData = data;
             allWeatherData()
+        }).catch(error => console.log(error));
+
+
+        fetch(requestUrl3)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Invalid location');
+            }
+            return response.json();
         })
-        .catch(error => console.log(error));
+        .then(data => {
+            console.log(data)
+            forecastData = data
+
+            allforecastData()
+            
+        }).catch(error => console.log(error));
+        
 }
 
 
 function allWeatherData(){
 
     const name = weatherData.name
-    const degrees = weatherData.main.temp
+    const degrees = Math.floor(weatherData.main.temp);
     const icon = weatherData.weather[0].icon
+    const iconDescript = weatherData.weather[0].description
     const iconUrl1 = `http://openweathermap.org/img/wn/${icon}@2x.png`;
     
     
     const locationName = document.querySelector('.location')
     const locationDegrees = document.querySelector('.degrees')
     const weatherLocation = document.querySelector('.weatherIcon')
+    const iconDescription = document.querySelector('.IconDescription')
+    
    
     locationName.textContent = `${name}`
-    locationDegrees.textContent = `${degrees}`
+    locationDegrees.textContent = `${degrees + '°'} `
     weatherLocation.innerHTML = `<img src="${iconUrl1}" alt="Weather Icon">`;
+    iconDescription.textContent = `${iconDescript}`
     
    
    
+}
+function allforecastData(){
+    const icons = forecastData.list[0].weather[0].icon
+    const icons1 = forecastData.list[1].weather[0].icon
+    const icons2 = forecastData.list[2].weather[0].icon
+    const icons3 = forecastData.list[3].weather[0].icon
+    
+
+
+
+    const iconUrl2 = `http://openweathermap.org/img/wn/${icons}@2x.png`;
+    const iconUrl3 = `http://openweathermap.org/img/wn/${icons1}@2x.png`;
+    const iconUrl4 = `http://openweathermap.org/img/wn/${icons2}@2x.png`;
+    const iconUrl5 = `http://openweathermap.org/img/wn/${icons3}@2x.png`;
+
+
+
+
+    one.innerHTML = `<img src="${iconUrl2}" alt="Weather Icon">`;
+    two.innerHTML = `<img src="${iconUrl3}" alt="Weather Icon">`;
+    three.innerHTML = `<img src="${iconUrl4}" alt="Weather Icon">`;
+    four.innerHTML = `<img src="${iconUrl5}" alt="Weather Icon">`;
 }
